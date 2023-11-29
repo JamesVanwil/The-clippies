33d6ed0963714c6821bff15f65bc408fec6cab25
JamesVanwil <james.vanwil@gmail.com>
2023-11-29 10:51:55 +0100

    web page update

---
+++ Prototype - Concept One/Assets/CloseWIndow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CloseWindow : MonoBehaviour, IPointerClickHandler
+{
+    public Image imageToDeactivate;
+
+    // Implement the click handler
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Check if the image to deactivate is not null
+        if (imageToDeactivate != null)
+        {
+            // Deactivate the specified image
+            imageToDeactivate.gameObject.SetActive(false);
+        }
+    }
+}
