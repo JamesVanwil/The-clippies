77072f468ddae268cb82c5c13add49ff78726940
JamesVanwil <james.vanwil@gmail.com>
2023-12-20 15:02:29 +0100

    ChangeSceneFrom3DView

---
+++ Prototype - Concept One/Assets/SceneChange.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    RaycastHit hit;
+    private bool canChangeScene;
+    public Camera PlayerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, PlayerCamera.transform.forward);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (hit.collider.CompareTag("Pc"))
+            {
+                canChangeScene = true;
+            }
+
+            else if (hit.collider.CompareTag("Phone"))
+            {
+                canChangeScene = true;
+
+            }
+            else
+            {
+                canChangeScene = false;
+            }
+        }
+        else
+        {
+            canChangeScene = false;
+        }
+
+        if (canChangeScene && Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+
+            if (hit.collider.CompareTag("Pc"))
+            {
+                SceneManager.LoadScene(2);
+            }
+            else if (hit.collider.CompareTag("Phone"))
+            {
+                SceneManager.LoadScene(1);
+
+            }
+        }
+    }
+}
--- Prototype - Concept One/Assets/Scripts/MainMenu.cs
+++ Prototype - Concept One/Assets/Scripts/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void BackTo3DView()
+    {
+        SceneManager.LoadScene(3);
+    }
 }
