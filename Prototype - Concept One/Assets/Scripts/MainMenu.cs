224446d560810eae08a7384c90196c8d3d6ab396
JamesVanwil <james.vanwil@gmail.com>
2024-01-20 12:37:35 +0100

    Notepad Update

--- Prototype - Concept One/Assets/Scripts/MainMenu.cs
+++ Prototype - Concept One/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -15,8 +17,12 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 
+
+
     public void BackTo3DView()
     {
+      
         SceneManager.LoadScene(3);
+
     }
 }
---
+++ Prototype - Concept One/Assets/Scripts/Notepad.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Notepad : MonoBehaviour
+{
+    public string TheText;
+    public GameObject ourNote;
+    public GameObject placeHolder;
+
+    private void Start()
+    {
+        // Check if placeHolder is assigned and has an InputField component
+        if (placeHolder != null)
+        {
+            InputField inputField = placeHolder.GetComponent<InputField>();
+            if (inputField != null)
+            {
+                TheText = PlayerPrefs.GetString("NoteContents");
+                inputField.text = TheText;
+            }
+            else
+            {
+                Debug.LogError("InputField component not found on placeHolder GameObject.");
+            }
+        }
+        else
+        {
+            Debug.LogError("placeHolder GameObject not assigned.");
+        }
+    }
+
+    public void SaveNotes()
+    {
+        TheText = ourNote.GetComponent<Text>().text;
+        PlayerPrefs.SetString("NoteContents", TheText);
+        PlayerPrefs.Save();
+    }
+}
